25f08b075d817a7ee922bcff9f097534d5213773
LaurenGifford <lauren.gifford.17@gmail.com>
2021-04-15 13:51:00 -0400

    adds tree binary search

---
+++ trees.js
@@ -0,0 +1,34 @@
+// FIRST PASS
+// function find(root, data) {
+//     let current = root
+//     let left = null
+//     let right = null
+//     if (data <= current) {
+//         left = data
+//     }
+//     else if (data > current ) {
+//         right = data
+//     }
+//     else {
+//         root = root + 1
+//         return find(root, data)
+//     }
+// }
+
+// CORRECT
+function findOrAdd(rootNode, newNode) {
+    let currentNode = rootnode.data
+    if (newNode.data < rootNode.data) {
+        currentNode = rootNode.left
+        if (currentNode) {
+            findOrAdd(currentNode, newNode)
+        }
+        else {currentNode.left = newNode}
+    }
+    else if (newNode.data > rootNode.data) {
+        currentNode = rootNode.right
+        if (currentNode) {
+            findOrAdd(currentNode, newNode)
+        } else {currentNode.right = newNode}
+    }
+}
