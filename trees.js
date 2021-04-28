42ac0ef942c2899a6060c26aa775c60378b99d1f
LaurenGifford <lauren.gifford.17@gmail.com>
2021-04-28 13:46:43 -0400

    function to check if two trees are the same

--- trees.js
+++ trees.js
@@ -115,3 +115,11 @@ function diameterOfBinaryTree(root) {
     }
 
 };
+
+// check if two trees are the same
+function isSameTree(p, q) {
+    if (!p && !q) return true
+    else if (!p || !q) return false
+    else if (p.val !== q.val) return false
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
